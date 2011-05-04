3ad7fcdd8e81868ea216c353d8c7190fabedd9f1
Alexandre Michetti Manduca <a.michetti@gmail.com>
2011-05-04 09:27:38 -0300

    Improved benchamerk. Now logs on the specified format.x

--- benchmark.js
+++ benchmark.js
@@ -2,19 +2,29 @@ var sys = require('sys');
 var WebSocket = require('websocket-client').WebSocket;
 var utils = require('./utils.js');
 
+var users = 0;
+
+var randomMessage = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's";
+
+function generateRandomMessage() {
+  var extraSize = parseInt(Math.random() * 100);
+  return "Benchmark Message: " + randomMessage.substring(0, extraSize);
+}
+
 function user() {
-  var now = new Date();
-  var testUserEmail = 'benchmark' + now + '@gmail.com';
+  var heartBeats = 0;
+  var testUserEmail = null;
   var ws = new WebSocket('ws://localhost:8080/socket.io/websocket');
   //var ws = new WebSocket('ws://173.203.127.245:8080/socket.io/websocket');
   
+  var tss = [];
   var joined = false;
   
   ws.onmessage = function(message) {
     var payload = utils.decode(message.data)[0];
   
     if (joined == false) {
-      console.log('Sending join message');
+      testUserEmail = 'user' + ++users + '@benchamrk.com';
       ws.send(utils.encode({action: 'join', email: testUserEmail}));
   
       joined = true;
@@ -23,22 +33,27 @@ function user() {
       var data = JSON.parse(payload.substr(3));
   
       if (data.action === 'join' && data.email === testUserEmail) {
-        console.log('Sending bechmark messages');
-  
         setInterval(function() {
-          ws.send(utils.encode({action: 'message', message: 'Benchmark message '}));
-          console.log('enviando');
+          tss.push(new Date());
+          ws.send(utils.encode({action: 'message', message: generateRandomMessage()}));
         }, 100);
   
-      } else if (data.action === 'message') { 
-          console.log('recebendo: ' + data.message);
+      } else if (data.action === 'message' && data.email === testUserEmail) { 
+          var d = new Date();
+          var ts = d.getDay() + '/' + d.getMonth() + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + ':' + d.getMilliseconds();
+
+          var sended = tss.shift();
+          var dt = d.getTime() - sended.getTime();
+
+          console.log(ts + ',' + users + ',' + payload.length + ',' + dt);
       }
+    } else if (payload.substr(0, 3) === '~h~') {
+      ws.send(utils.encode('~h~' + ++heartBeats));
     }
-  
   }
 }
 
-for(var i=1; i<=20; i++) {
+for(var i=1; i<=50; i++) {
   console.log('creating user ' + i);
 
   setTimeout(function() {
